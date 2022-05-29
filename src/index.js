39426e158f194751c5a2b1a8658c566b9123dbca
Afikzk <100334286+Afikzk@users.noreply.github.com>
2022-05-29 13:43:21 +0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import "./index.css";
+import "./components/Navbar"
+import Navbar from "./components/Navbar";
+
+ReactDOM.render(
+  <div>
+    <Navbar />
+    <p>WIP</p>
+    <App />
+  </div>,
+  document.getElementById("root")
+  );
